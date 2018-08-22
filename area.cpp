28d4dea90e8634ada280fa8f2d04a5f5603f1f11
milkyway002 <sea_bom_12@naver.com>
2018-08-22 17:00:12 +0900

    2 problem

---
+++ area.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	int height, width;
+	scanf("%d %d", &width, &height);
+	printf("%d", width*height);
+} 
---
+++ swap.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	int num1, num2, temp;
+	scanf("%d %d", &num1, &num2);
+	
+}
